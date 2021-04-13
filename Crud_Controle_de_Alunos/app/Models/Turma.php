daca7093a28eb3edb9888636d1943a024e1ed525
Saulo de Castro <sdc2decastro@gmail.com>
2021-04-13 03:10:56 -0300

    Criação dos modelos e migrations

---
+++ Crud_Controle_de_Alunos/app/Models/Turma.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Turma extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'ano',
+        'nivel',
+        'serie',
+        'turno',
+        'escola_id'
+    ];
+
+    public function Escola()
+    {
+        return $this->belongsTo(Escola::class)->withTrashed();
+    }
+
+    public function Alunos()
+    {
+        return $this->belongsToMany(Alunos::class)->withTrashed();        
+    }
+}
